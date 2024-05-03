b333dc445f26fd38b75ff27419a900ecf4ed95ea
Darkkid819 <darkkid8192004@gmail.com>
2024-05-03 17:05:12 -0400

    offload rendering to GPU

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 int mandelbrot(float x, float y, int maxIterations) {
     float zx = 0.0, zy = 0.0, zx2 = 0.0, zy2 = 0.0;
     int iterations = 0;
-
     while (zx2 + zy2 < 4.0 && iterations < maxIterations) {
         zy = 2.0 * zx * zy + y;
         zx = zx2 - zy2 + x;
@@ -11,16 +10,17 @@ int mandelbrot(float x, float y, int maxIterations) {
         zy2 = zy * zy;
         iterations++;
     }
-
     return iterations;
 }
 
 int main() {
     int screenWidth = 800;
     int screenHeight = 450;
-
     InitWindow(screenWidth, screenHeight, "Mandelbrot Set");
+
     SetTargetFPS(60);
+
+    RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
 
     float scale = 4.0;
     Vector2 center = {0.0, 0.0};
@@ -34,22 +34,27 @@ int main() {
         float wheelMove = GetMouseWheelMove();
         scale *= (1 - wheelMove * 0.1);
 
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        for (int y = 0; y < screenHeight; y++) {
-            for (int x = 0; x < screenWidth; x++) {
-                float scaledX = (x - screenWidth / 2.0) * scale / screenWidth + center.x;
-                float scaledY = (y - screenHeight / 2.0) * scale / screenHeight + center.y;
-                int color = mandelbrot(scaledX, scaledY, 1000);
-                DrawPixel(x, y, ColorFromHSV(color % 256, 1.0, color < 1000 ? 1.0 : 0));
+        BeginTextureMode(target);
+            ClearBackground(BLANK);
+            for (int y = 0; y < screenHeight; y++) {
+                for (int x = 0; x < screenWidth; x++) {
+                    float scaledX = (x - screenWidth / 2.0) * scale / screenWidth + center.x;
+                    float scaledY = (y - screenHeight / 2.0) * scale / screenHeight + center.y;
+                    int color = mandelbrot(scaledX, scaledY, 1000);
+                    DrawPixel(x, y, ColorFromHSV(color % 256, 1.0, color < 1000 ? 1.0 : 0));
+                }  
             }
-        }
+        EndTextureMode();
 
-        DrawFPS(10, 10);
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawTextureRec(target.texture, (Rectangle){ 0, 0, target.texture.width, -target.texture.height }, (Vector2){ 0, 0 }, WHITE);
+            DrawFPS(10, 10);
         EndDrawing();
     }
 
+    UnloadRenderTexture(target);
     CloseWindow();
+
     return 0;
 }
